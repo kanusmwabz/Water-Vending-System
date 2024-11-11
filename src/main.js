e5eb4ea241b74e8677adbe841ce4831f6d6ad627
Nathaniel Mwabili <nathaniel.mwabz@gmail.com>
2024-11-11 14:30:34 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/components/services.css";
+import "../styles/components/footer.css";
+import "../styles/components/mobile-nav.css";
+
+import "../styles/utils.css";
+
+import mobileNav from "./utils/mobile-nav";
+
+mobileNav();
